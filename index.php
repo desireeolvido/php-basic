5f824785d9fb83d8acec8e81f9e5e3248ea09ddb
=desireeolvido <desireeolvido21@gmail.com>
2022-10-23 08:31:20 +0800

    initial commit or whatever message for first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Basic</title>
+</head>
+<body>
+    <?php
+        echo "Hello World, I am Desiree S. Olvido";
+    ?>
+</body>
+</html>
